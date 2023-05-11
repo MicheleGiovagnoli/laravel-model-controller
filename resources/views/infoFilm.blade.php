ebbd0fc4caea597ed42f59f58e30ca065af8cbc0
michele <michele.giovagnoli1@gmail.com>
2023-05-11 17:42:08 +0200

    Stilizzazione scss

--- resources/views/infoFilm.blade.php
+++ resources/views/infoFilm.blade.php
@@ -3,9 +3,9 @@
 @section('page_title', 'Ciaoo')
 
 @section('content')
-    <main class="d-flex flex-wrap justify-content-around">
+    <main class="container">
         @foreach ($movies as $movie)
-            <div class="card" style="width: 18rem;">
+            <div class="card myCard" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">Titolo: {{$movie->title}}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary">Titolo originale: {{$movie->original_title}}</h6>
